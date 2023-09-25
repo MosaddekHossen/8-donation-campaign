58e2b9112d8cba9ed5d7573fa1e8802b7b016b5f
MosaddekHossen <mosaddekhossen7496@gmail.com>
2023-09-26 03:00:46 +0600

    Dynamic card color change

--- src/components/Pages/Home/Card.jsx
+++ src/components/Pages/Home/Card.jsx
@@ -1,15 +1,15 @@
 import { Link } from "react-router-dom";
 
 const Card = ({ card }) => {
-    const { image, Card_bg, id, Category,Title } = card || {}
+    const { image, Card_bg, text, bg, id, Category, Title } = card || {}
     console.log(Card_bg)
     return (
         <div>
             <Link to={`/details/${id}`}>
                 <div className={`bg-[${Card_bg}] bg-opacity-[15%] rounded-md hover:cursor-pointer`}>
                     <img className="w-full" src={image} alt="Image" />
-                    <h3 className={`my-2 bg-[#0052FF] ml-4 rounded-md mt-4 py-1 px-5 inline-block`}>{Category}</h3>
-                    <p className="text-2xl ml-4 pb-4 font-semibold">{Title}</p>
+                    <h3 className={`my-2 text-[${text}] bg-[${bg}] ml-4 rounded-md mt-4 py-1 px-5 inline-block`}>{Category}</h3>
+                    <p className={`text-2xl text-[${text}] ml-4 pb-4 font-semibold`}>{Title}</p>
                 </div>
             </Link>
         </div>
